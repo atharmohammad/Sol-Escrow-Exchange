3306cfa03a0ea4f9efacfcbe673618e8071534ef
Athar <mohd.rule123@gmail.com>
2022-09-27 10:50:37 +0530

    transferring ownership of escrow and creating it a pda

--- program/src/processor.rs
+++ program/src/processor.rs
@@ -4,13 +4,14 @@ use solana_program::{
     pubkey::Pubkey,
     account_info::{AccountInfo,next_account_info},
     msg,
-    program_error::ProgramError, rent::Rent, sysvar::Sysvar
+    program_error::ProgramError, rent::Rent, sysvar::Sysvar, bpf_loader::id, program::invoke
 };
+use spl_token::instruction::set_authority;
 use crate::{instructions::*, error::EscrowError};
 use crate::state::*;
 
 pub fn process_instruction(
-    program_id:&Pubkey,
+    _program_id:&Pubkey,
     accounts:&[AccountInfo],
     input:&[u8]
 ) -> ProgramResult {
@@ -33,11 +34,28 @@ pub fn process_instruction(
             if !rent.is_exempt(escrow_wallet.lamports(), escrow_wallet.data_len()) {
                 return Err(ProgramError::AccountNotRentExempt);
             }
-            let escrow_info = Escrow::try_from_slice(&escrow_wallet.data.borrow())?;
+            let mut escrow_info = Escrow::try_from_slice(&escrow_wallet.data.borrow())?;
             let token_program = next_account_info(accounts_iter)?;
             if escrow_info.is_initialized {
                 return Err(EscrowError::EscrowAlreadyInitialized.into());
             }
+            escrow_info.user_sender = *user_sender.key;
+            escrow_info.is_initialized = true;
+            escrow_info.expected_amount = amount;
+            escrow_info.escrow_token_account = *escrow_token_account.key;
+            escrow_info.senders_token_receiver_account = *senders_token_account.key;
+            // creating pda for escrow token account
+            let (pda,_bump) = Pubkey::find_program_address(&[b"token"], &id());
+            // transferring ownership of pda to program from sender to use this account as an escrow for sending Y tokens to Bob
+            let ownership_inst = set_authority(token_program.key, escrow_token_account.key, Some(&pda), spl_token::instruction::AuthorityType::AccountOwner, &id(), &[&user_sender.key])?;
+            invoke(&ownership_inst, 
+                &[
+                    token_program.clone(),
+                    escrow_token_account.clone(),
+                    user_sender.clone()
+                ]
+            )?;
+            
             Ok(())
         }
     }
--- program/src/state.rs
+++ program/src/state.rs
@@ -1,13 +1,12 @@
 use solana_program :: {
     pubkey::Pubkey
 };
-use borsh::{BorshDeserialize, BorshSerialize};
+use borsh::{BorshDeserialize, BorshSerialize, BorshSchema};
 
-#[derive(Clone,Copy,Debug,BorshSerialize,BorshDeserialize,PartialEq)]
+#[derive(Clone,Copy,Debug,BorshSerialize,BorshDeserialize,BorshSchema,PartialEq)]
 pub struct Escrow {
     pub user_sender : Pubkey,
     pub is_initialized : bool,
-    pub user_receiver : Pubkey,
     pub escrow_token_account : Pubkey,
     pub senders_token_receiver_account : Pubkey,
     pub expected_amount : u64
